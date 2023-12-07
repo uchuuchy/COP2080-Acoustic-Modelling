289f677b8fb7ebbde16ecc21d45859aee4f5e159
Valerio Sardinas <vsardinas3101@floridapoly.edu>
2023-12-07 11:36:13 -0500

    Fixed new wave form being added ontop the previously created one

--- modules/WaveManip.py
+++ modules/WaveManip.py
@@ -12,7 +12,7 @@ class WaveManip:
         self.__time = np.linspace(0, len(self.__signal) / self.__fs, num=len(self.__signal))
 
     def wave_plot(self):
-        plt.figure(1).set_size_inches(4.3125, 3.219)
+        plt.figure().set_size_inches(4.3125, 3.219)
         plt.title(f"Wave Form")
         plt.plot(self.__time, self.__signal)
         plt.savefig(f"./assets/wave_form.png")
